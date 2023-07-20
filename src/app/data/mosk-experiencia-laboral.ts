71e8ee4e2df33390755ca7d527793e5d2c99048c
Julian <julian.meneses11@gmail.com>
2023-07-20 08:24:03 -0300

    actualizacion exp laboral

--- src/app/data/mosk-experiencia-laboral.ts
+++ src/app/data/mosk-experiencia-laboral.ts
@@ -4,13 +4,14 @@ export const Experiencias: Experiencia []  = [ // Hacemos que la variable Experi
 
     {
         id: 1,
-        puesto: "App Developer (Power Platform)",
+        puesto: "Power Platform Developer",
         nombre_empresa: "Procom IT Solutions",
         url: "https://procomitsolutions.com/es",
         fecha_inicio: "Septiembre 2022",
         fecha_fin: "Actualidad",
-        descripcion: `Me desempeño como desarrollador de apps para escritorio y móviles y automatizador de procesos empresariales a través de las herramientas Power Apps, Power Automate, Dataverse y Sharepoint de Power Platform, perteneciente a Microsoft.
-        Trabajo en un equipo interdisciplinario con la metodología Scrum, teniendo reuniones diarias y sprints cada dos semanas.`,
+        descripcion: `Me desempeño como automatizador de procesos empresariales y como desarrollador de aplicaciones, estando a cargo 
+        tanto de la funcionalidad como del diseño. Para ello uso las herramientas Power Apps, Power Automate, Dataverse, Sharepoint y Power BI de Power Platform, perteneciente a Microsoft.
+        Trabajo en un equipo bajo un líder, con reuniones diarias.`,
         posicion_Y: "exp-bar-Y--0"
             
     },    
